2dec0323bcf9b1eac2d184394604655b5d5218ef
Anton Ganhammar <ganhammar@gmail.com>
2023-09-23 13:15:24 +0200

    feat: add project base

---
+++ src/App.Login/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace App.Login;
+
+public static class Constants
+{
+  public const string BasePath = "/api/login";
+  public const string FromEmail = "hello@wdid.fyi";
+  public const string Origin = "https://www.wdid.fyi/";
+}
---
+++ src/App.Login/Infrastructure/Response.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation.Results;
+
+namespace App.Login.Infrastructure;
+
+public class Response<T> : Response, IResponse<T>
+    where T : new()
+{
+  public T? Result
+  {
+    get => IsValid ? ResultObject : default;
+    set
+    {
+      if (value != null)
+      {
+        ResultObject = value;
+      }
+    }
+  }
+
+  private T ResultObject { get; set; } = new T();
+}
+
+public class Response : IResponse
+{
+  public Response()
+  {
+    Errors = Enumerable.Empty<ValidationFailure>();
+  }
+
+  public bool IsValid
+  {
+    get => !Errors.Any();
+  }
+
+  public IEnumerable<ValidationFailure> Errors { get; set; }
+}
