be9e849c5f44523c88a186f420bbba4f60fbc7cb
Anton Ganhammar <ganhammar@gmail.com>
2023-10-15 09:25:41 +0200

    feat: add powertools

--- src/App.Login/LambdaEntryPoint.cs
+++ src/App.Login/LambdaEntryPoint.cs
@@ -1,4 +1,9 @@
-﻿using Amazon.Lambda.AspNetCoreServer;
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.AspNetCoreServer;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using AWS.Lambda.Powertools.Logging;
+using AWS.Lambda.Powertools.Tracing;
 
 namespace App.Login;
 
@@ -15,4 +20,22 @@ public class LambdaEntryPoint : APIGatewayProxyFunction
   }
 
   protected override void Init(IHostBuilder builder) { }
+
+  private Dictionary<string, string> _defaultDimensions = new()
+  {
+    {"Environment", Environment.GetEnvironmentVariable("ENVIRONMENT") ??  "Unknown"},
+    {"Runtime",Environment.Version.ToString()}
+  };
+
+  [LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
+  [Logging(CorrelationIdPath = CorrelationIdPaths.ApiGatewayRest, LogEvent = true)]
+  [Tracing]
+  public override Task<APIGatewayProxyResponse> FunctionHandlerAsync(
+    APIGatewayProxyRequest request, ILambdaContext lambdaContext)
+  {
+    if (!_defaultDimensions.ContainsKey("Version"))
+      _defaultDimensions.Add("Version", lambdaContext.FunctionVersion ?? "Unknown");
+
+    return base.FunctionHandlerAsync(request, lambdaContext);
+  }
 }
