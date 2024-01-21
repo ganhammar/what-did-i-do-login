cb38decc4128b3b3fcf93de2a8bd545f252d0496
Anton Ganhammar <ganhammar@gmail.com>
2024-01-21 10:55:12 +0100

    fix: tests

--- test/App.Login.Tests/Features/Authorize/AuthorizationControllerTests.cs
+++ test/App.Login.Tests/Features/Authorize/AuthorizationControllerTests.cs
@@ -27,177 +27,177 @@ public class AuthorizationControllerTests : TestBase
 
   [Fact]
   public async Task Should_ReturnSignInResult_When_AuthorizationRequestIsValid() => await ControllerTest<AuthorizationController>(
+    // Arrange
+    ConfigureController,
+    // Act & Assert
+    async (controller, services) =>
+    {
       // Arrange
-      ConfigureController,
-      // Act & Assert
-      async (controller, services) =>
-      {
-        // Arrange
-        await CreateAndLoginValidUser(services);
+      await CreateAndLoginValidUser(services);
 
-        // Act
-        var result = await controller.Authorize(new());
+      // Act
+      var result = await controller.Authorize(new());
 
-        // Assert
-        Assert.NotNull(result);
+      // Assert
+      Assert.NotNull(result);
 
-        var signInResult = result as SignInResult;
-        Assert.NotNull(signInResult);
-      });
+      var signInResult = result as SignInResult;
+      Assert.NotNull(signInResult);
+    });
 
   [Fact]
   public async Task Should_ReturnChallenge_When_UserIsntAuthenticated() => await ControllerTest<AuthorizationController>(
+    // Arrange
+    ConfigureController,
+    // Act & Assert
+    async (controller, services) =>
+    {
       // Arrange
-      ConfigureController,
-      // Act & Assert
-      async (controller, services) =>
+      var mediator = services.GetRequiredService<IMediator>();
+      var httpContext = GetMock<HttpContext>();
+      var featureCollection = new FeatureCollection();
+      featureCollection.Set(new OpenIddictServerAspNetCoreFeature
       {
-        // Arrange
-        var mediator = services.GetRequiredService<IMediator>();
-        var httpContext = GetMock<HttpContext>();
-        var featureCollection = new FeatureCollection();
-        featureCollection.Set(new OpenIddictServerAspNetCoreFeature
+        Transaction = new OpenIddictServerTransaction
         {
-          Transaction = new OpenIddictServerTransaction
-          {
-            Request = new OpenIddictRequest(),
-          },
-        });
-        httpContext!.Setup(x => x.Features).Returns(featureCollection);
-
-        // Act
-        var result = await controller.Authorize(new());
-
-        // Assert
-        Assert.NotNull(result);
-
-        var challengeResult = result as ChallengeResult;
-        Assert.NotNull(challengeResult);
+          Request = new OpenIddictRequest(),
+        },
       });
+      httpContext!.Setup(x => x.Features).Returns(featureCollection);
+
+      // Act
+      var result = await controller.Authorize(new());
+
+      // Assert
+      Assert.NotNull(result);
+
+      var challengeResult = result as ChallengeResult;
+      Assert.NotNull(challengeResult);
+    });
 
   [Fact]
   public async Task Should_ReturnForbid_When_AuthorizeCommandIsntValid() => await ControllerTest<AuthorizationController>(
+    // Arrange
+    ConfigureController,
+    // Act & Assert
+    async (controller, services) =>
+    {
       // Arrange
-      ConfigureController,
-      // Act & Assert
-      async (controller, services) =>
+      var mediator = services.GetRequiredService<IMediator>();
+      var httpContext = GetMock<HttpContext>();
+      var featureCollection = new FeatureCollection();
+      featureCollection.Set(new OpenIddictServerAspNetCoreFeature
       {
-        // Arrange
-        var mediator = services.GetRequiredService<IMediator>();
-        var httpContext = GetMock<HttpContext>();
-        var featureCollection = new FeatureCollection();
-        featureCollection.Set(new OpenIddictServerAspNetCoreFeature
+        Transaction = new OpenIddictServerTransaction
         {
-          Transaction = new OpenIddictServerTransaction
+          Request = new OpenIddictRequest
           {
-            Request = new OpenIddictRequest
-            {
-              Prompt = Prompts.None,
-            },
+            Prompt = Prompts.None,
           },
-        });
-        httpContext!.Setup(x => x.Features).Returns(featureCollection);
-
-        // Act
-        var result = await controller.Authorize(new());
-
-        // Assert
-        Assert.NotNull(result);
-
-        var forbidResult = result as ForbidResult;
-        Assert.NotNull(forbidResult);
+        },
       });
+      httpContext!.Setup(x => x.Features).Returns(featureCollection);
+
+      // Act
+      var result = await controller.Authorize(new());
+
+      // Assert
+      Assert.NotNull(result);
+
+      var forbidResult = result as ForbidResult;
+      Assert.NotNull(forbidResult);
+    });
 
   [Fact]
   public async Task Should_Return_When_LoggingOut() => await ControllerTest<AuthorizationController>(
-      // Arrange
-      ConfigureController,
-      // Act & Assert
-      async (controller, services) =>
-      {
-        // Act
-        var result = await controller.LogoutCommand(new());
+    // Arrange
+    ConfigureController,
+    // Act & Assert
+    async (controller, services) =>
+    {
+      // Act
+      var result = await controller.Logout(new());
 
-        // Assert
-        Assert.NotNull(result);
+      // Assert
+      Assert.NotNull(result);
 
-        var signOutResult = result as SignOutResult;
-        Assert.NotNull(signOutResult);
-      });
+      var noContentResult = result as NoContentResult;
+      Assert.NotNull(noContentResult);
+    });
 
   [Fact]
   public async Task Should_ReturnSignInResult_When_ExchangeCommandRequestIsValid() => await ControllerTest<AuthorizationController>(
+    // Arrange
+    ConfigureController,
+    // Act & Assert
+    async (controller, services) =>
+    {
       // Arrange
-      ConfigureController,
-      // Act & Assert
-      async (controller, services) =>
+      var applicationManager = services.GetRequiredService<IOpenIddictApplicationManager>();
+      var clientId = Guid.NewGuid().ToString();
+      var clientSecret = Guid.NewGuid().ToString();
+      var application = new OpenIddictDynamoDbApplication
       {
-        // Arrange
-        var applicationManager = services.GetRequiredService<IOpenIddictApplicationManager>();
-        var clientId = Guid.NewGuid().ToString();
-        var clientSecret = Guid.NewGuid().ToString();
-        var application = new OpenIddictDynamoDbApplication
+        ClientId = clientId,
+      };
+      await applicationManager.CreateAsync(application, clientSecret);
+      var httpContext = GetMock<HttpContext>();
+      var featureCollection = new FeatureCollection();
+      featureCollection.Set(new OpenIddictServerAspNetCoreFeature
+      {
+        Transaction = new OpenIddictServerTransaction
         {
-          ClientId = clientId,
-        };
-        await applicationManager.CreateAsync(application, clientSecret);
-        var httpContext = GetMock<HttpContext>();
-        var featureCollection = new FeatureCollection();
-        featureCollection.Set(new OpenIddictServerAspNetCoreFeature
-        {
-          Transaction = new OpenIddictServerTransaction
+          Request = new OpenIddictRequest
           {
-            Request = new OpenIddictRequest
-            {
-              ClientId = clientId,
-              ClientSecret = clientSecret,
-              GrantType = GrantTypes.ClientCredentials,
-              Scope = "test",
-            },
+            ClientId = clientId,
+            ClientSecret = clientSecret,
+            GrantType = GrantTypes.ClientCredentials,
+            Scope = "test",
           },
-        });
-        httpContext!.Setup(x => x.Features).Returns(featureCollection);
-
-        // Act
-        var result = await controller.ExchangeCommand(new());
-
-        // Assert
-        Assert.NotNull(result);
-
-        var signInResult = result as SignInResult;
-        Assert.NotNull(signInResult);
+        },
       });
+      httpContext!.Setup(x => x.Features).Returns(featureCollection);
+
+      // Act
+      var result = await controller.Exchange(new());
+
+      // Assert
+      Assert.NotNull(result);
+
+      var signInResult = result as SignInResult;
+      Assert.NotNull(signInResult);
+    });
 
   [Fact]
   public async Task Should_ReturnForbid_When_ExchangeCommandRequestIsntValid() => await ControllerTest<AuthorizationController>(
+    // Arrange
+    ConfigureController,
+    // Act & Assert
+    async (controller, services) =>
+    {
       // Arrange
-      ConfigureController,
-      // Act & Assert
-      async (controller, services) =>
+      var mediator = services.GetRequiredService<IMediator>();
+      var httpContext = GetMock<HttpContext>();
+      var featureCollection = new FeatureCollection();
+      featureCollection.Set(new OpenIddictServerAspNetCoreFeature
       {
-        // Arrange
-        var mediator = services.GetRequiredService<IMediator>();
-        var httpContext = GetMock<HttpContext>();
-        var featureCollection = new FeatureCollection();
-        featureCollection.Set(new OpenIddictServerAspNetCoreFeature
+        Transaction = new OpenIddictServerTransaction
         {
-          Transaction = new OpenIddictServerTransaction
+          Request = new OpenIddictRequest
           {
-            Request = new OpenIddictRequest
-            {
-              Prompt = Prompts.None,
-            },
+            Prompt = Prompts.None,
           },
-        });
-        httpContext!.Setup(x => x.Features).Returns(featureCollection);
-
-        // Act
-        var result = await controller.ExchangeCommand(new());
-
-        // Assert
-        Assert.NotNull(result);
-
-        var forbidResult = result as ForbidResult;
-        Assert.NotNull(forbidResult);
+        },
       });
+      httpContext!.Setup(x => x.Features).Returns(featureCollection);
+
+      // Act
+      var result = await controller.Exchange(new());
+
+      // Assert
+      Assert.NotNull(result);
+
+      var forbidResult = result as ForbidResult;
+      Assert.NotNull(forbidResult);
+    });
 }
